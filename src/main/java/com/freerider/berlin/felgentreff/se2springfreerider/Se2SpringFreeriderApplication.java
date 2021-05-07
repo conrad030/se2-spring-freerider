3df115c062146a998a2c89b07263dc7a88836884
Conrad <conrad.felgentreff1@gmail.com>
2021-05-07 16:14:46 +0200

    Added Basics

---
+++ src/main/java/com/freerider/berlin/felgentreff/se2springfreerider/Se2SpringFreeriderApplication.java
@@ -0,0 +1,13 @@
+package com.freerider.berlin.felgentreff.se2springfreerider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se2SpringFreeriderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Se2SpringFreeriderApplication.class, args);
+	}
+
+}
