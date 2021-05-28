e86a20ef685663b07682146145ba09c546a8f6b7
conrad030 <conrad.felgentreff@web.de>
2021-05-28 16:27:05 +0200

    Added implementation in CustomerRepository and tests in doWhenApplicationReady()-method in Se2SpringFreeriderApplication

--- src/main/java/com/freerider/berlin/felgentreff/se2springfreerider/Se2SpringFreeriderApplication.java
+++ src/main/java/com/freerider/berlin/felgentreff/se2springfreerider/Se2SpringFreeriderApplication.java
@@ -1,10 +1,91 @@
 package com.freerider.berlin.felgentreff.se2springfreerider;
 
+import java.util.ArrayList;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import de.freerider.model.Customer;
+import de.freerider.repository.CustomerRepository;
 
 @SpringBootApplication
 public class Se2SpringFreeriderApplication {
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void doWhenApplicationReady() {
+		Customer customer1 = new Customer("Martin", "Aston", "a@b.com");
+		customer1.setId("123hew3er23fbf");
+		Customer customer2 = new Customer("Benz", "Mercedes", "a@b.com");
+		customer2.setId("123hew3er23fc1");
+		Customer customer3 = new Customer("Laren", "Mc", "a@b.com");
+		customer3.setId("123hew3er23fc2");
+		Customer customer4 = new Customer("Royce", "Rolls", "a@b.com");
+		customer4.setId("123hew3er23fc3");
+		Customer customer5 = new Customer("MW", "B", "a@b.com");
+
+		CustomerRepository repository = new CustomerRepository();
+
+		// Should be 0
+		System.out.println(repository.count());
+
+		repository.save(customer1);
+		// Should be 1
+		System.out.println(repository.count());
+
+		ArrayList<Customer> entities = new ArrayList<>();
+		entities.add(customer2);
+		entities.add(customer3);
+		entities.add(customer4);
+		repository.saveAll(entities);
+
+		// Should be 4
+		System.out.println(repository.count());
+
+		// Should be true
+		System.out.println(repository.findById(customer1.getId()).get().getId().equals(customer1.getId()));
+
+		// Should be true
+		System.out.println(repository.existsById(customer1.getId()));
+
+		// Should contain 4 values
+		System.out.println(repository.findAll());
+
+		ArrayList<String> ids = new ArrayList<>();
+		ids.add(customer1.getId());
+		ids.add(customer2.getId());
+		ids.add("123test");
+		// Should contain 2 values
+		System.out.println(repository.findAllById(ids));
+
+		repository.deleteById(customer1.getId());
+		// Should be 3
+		System.out.println(repository.count());
+
+		repository.delete(customer2);
+		// Should be 2
+		System.out.println(repository.count());
+
+		entities.clear();
+		entities.add(customer2);
+		entities.add(customer3);
+		repository.deleteAll(entities);
+		// Should be 1
+		System.out.println(repository.count());
+
+		repository.deleteAll();
+		// Should be 0
+		System.out.println(repository.count());
+
+		repository.save(customer1);
+		// Should be 1
+		System.out.println(repository.count());
+		repository.save(customer1);
+		// Should still be 1
+		System.out.println(repository.count());
+		repository.save(customer1);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Se2SpringFreeriderApplication.class, args);
--- src/main/java/de/freerider/repository/CustomerRepository.java
+++ src/main/java/de/freerider/repository/CustomerRepository.java
@@ -1,88 +1,132 @@
 package de.freerider.repository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Optional;
 import de.freerider.model.Customer;
 
 import org.springframework.stereotype.Component;
 
-
 @Component
-class CustomerRepository implements CrudRepository<Customer, String> {
+public class CustomerRepository implements CrudRepository<Customer, String> {
 	//
-	private final IDGenerator idGen = new IDGenerator( "C", IDGenerator.IDTYPE.NUM, 6 );
+	private final IDGenerator idGen = new IDGenerator("C", IDGenerator.IDTYPE.NUM, 6);
+	private HashMap<String, Customer> entities;
 
-	@Override
-	public <S extends Customer> S save(S entity) {
-		// TODO Auto-generated method stub
-		return null;
+	public CustomerRepository() {
+		this.entities = new HashMap<>();
 	}
 
 	@Override
-	public <S extends Customer> Iterable<S> saveAll(Iterable<S> entities) {
-		// TODO Auto-generated method stub
-		return null;
+	public <S extends Customer> S save(S entity) throws IllegalArgumentException {
+		if (entity != null) {
+			if (entity.getId() == null || entity.getId().equals("")) {
+				entity.setId(this.idGen.nextId());
+				while (true) {
+					Customer existingEntity = this.entities.get(entity.getId());
+					if (existingEntity == null) {
+						this.entities.put(entity.getId(), entity);
+						return entity;
+					}
+				}
+			} else {
+				this.entities.put(entity.getId(), entity);
+				return entity;
+			}
+		} else {
+			throw new IllegalArgumentException();
+		}
 	}
 
 	@Override
-	public Optional<Customer> findById(String id) {
-		// TODO Auto-generated method stub
-		return null;
+	public <S extends Customer> Iterable<S> saveAll(Iterable<S> entities) throws IllegalArgumentException {
+		for (Customer entity: entities) {
+			entity = this.save(entity);
+		}
+		return entities;
 	}
 
 	@Override
-	public boolean existsById(String id) {
-		// TODO Auto-generated method stub
-		return false;
+	public Optional<Customer> findById(String id) throws IllegalArgumentException {
+		if (id != null) {
+			Customer entity = this.entities.get(id);
+			Optional<Customer> returnValue;
+			if (entity == null) {
+				returnValue = Optional.empty();
+			} else {
+				returnValue = Optional.of(entity);
+			}
+			return returnValue;
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	@Override
+	public boolean existsById(String id) throws IllegalArgumentException {
+		if (this.findById(id).isPresent()) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public Iterable<Customer> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.entities.values();
 	}
 
 	@Override
-	public Iterable<Customer> findAllById(Iterable<String> ids) {
-		// TODO Auto-generated method stub
-		return null;
+	public Iterable<Customer> findAllById(Iterable<String> ids) throws IllegalArgumentException {
+		ArrayList<Customer> returnValues = new ArrayList<>();
+		for (String id: ids) {
+			if (this.existsById(id)) {
+				returnValues.add(this.findById(id).get());
+			}
+		}
+		return returnValues;
 	}
 
 	@Override
 	public long count() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.entities.size();
 	}
 
 	@Override
-	public void deleteById(String id) {
-		// TODO Auto-generated method stub
-		
+	public void deleteById(String id) throws IllegalArgumentException {
+		if (id != null) {
+			this.entities.remove(id);
+		} else {
+			throw new IllegalArgumentException();
+		}
 	}
 
 	@Override
 	public void delete(Customer entity) {
-		// TODO Auto-generated method stub
-		
+		if (entity != null) {
+			this.deleteById(entity.getId());
+		} else {
+			throw new IllegalArgumentException();
+		}
 	}
 
 	@Override
-	public void deleteAllById(Iterable<? extends String> ids) {
-		// TODO Auto-generated method stub
-		
+	public void deleteAllById(Iterable<? extends String> ids) throws IllegalArgumentException {
+		for (String id: ids) {
+			this.deleteById(id);
+		}
 	}
 
 	@Override
-	public void deleteAll(Iterable<? extends Customer> entities) {
-		// TODO Auto-generated method stub
-		
+	public void deleteAll(Iterable<? extends Customer> entities) throws IllegalArgumentException {
+		for (Customer entity: entities) {
+			this.delete(entity);
+		}
 	}
 
 	@Override
 	public void deleteAll() {
-		// TODO Auto-generated method stub
-		
+		this.entities.clear();
 	}
 
-
-	
 }
